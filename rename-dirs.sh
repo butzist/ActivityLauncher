f220f99d892f932632d49fee0034d4691d3af3f4
Adam Szalkowski <adam@szalkowski.de>
2017-04-01 10:21:48 +0200

    Rename app

--- rename-dirs.sh
+++
@@ -1,8 +0,0 @@
-for d in ?? ??-??; do
-   mv $d values-$(echo $d | sed 's/-/-r/')
-done
-mv values-sv-rSE values-sv
-mv values-pt-rPT values-pt
-#mv values-la-rLA values-la
-mv values-zh-rTW values-zh
-mv values-es-rES values-es
