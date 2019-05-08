dfb7fe271dae9379b5453bbb6e88f30a1adc94a9
Adam Szalkowski <adam@szalkowski.de>
2019-05-08 14:10:49 +0200

    Migrate to AndroidX

--- ActivityLauncherApp/src/main/java/de/szalkowski/activitylauncher/MainActivity.java
+++ ActivityLauncherApp/src/main/java/de/szalkowski/activitylauncher/MainActivity.java
@@ -7,9 +7,9 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
