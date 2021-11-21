c6875668b2c50fc9c7d176deae76417d599b0e38
oldherl <oldherl@gmail.com>
2021-11-21 17:02:21 +0800

    fix broken build caused by missing import

--- ActivityLauncherApp/src/main/java/de/szalkowski/activitylauncher/MainActivity.java
+++ ActivityLauncherApp/src/main/java/de/szalkowski/activitylauncher/MainActivity.java
@@ -2,6 +2,7 @@ package de.szalkowski.activitylauncher;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
