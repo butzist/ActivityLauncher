169d1fe3ec2f7855e5910009f11dbad694a6471a
Ismael <ismael034@correo.ugr.es>
2022-01-03 10:34:42 +0100

    Fix low contrast in itemList

--- ActivityLauncherApp/src/main/java/de/szalkowski/activitylauncher/AllTasksListAdapter.java
+++ ActivityLauncherApp/src/main/java/de/szalkowski/activitylauncher/AllTasksListAdapter.java
@@ -106,7 +106,9 @@ public class AllTasksListAdapter extends BaseExpandableListAdapter implements Fi
 
         if (activity.is_private) {
             text1.setEnabled(false);
+            text1.setAlpha(0.5f);
             text2.setEnabled(false);
+            text2.setAlpha(0.5f);
 
             ImageView icon1 = view.findViewById(android.R.id.icon1);
             icon1.setVisibility(View.VISIBLE);
