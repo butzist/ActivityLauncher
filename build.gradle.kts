fbd979457c186d58c4acc18c8c509803d54a2155
Adam M. Szalkowski <adam@szalkowski.de>
2024-01-06 08:23:27 +0100

    chore: upgrade gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.2.0" apply false
+    id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.google.dagger.hilt.android") version "2.50" apply false
 }
