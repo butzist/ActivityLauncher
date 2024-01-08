bef165084be3a86142e1286330cdddca9f22758f
Adam M. Szalkowski <adam@szalkowski.de>
2024-01-08 06:40:13 +0100

    fix: do not reload packages per fragment

--- app/src/main/java/de/szalkowski/activitylauncher/services/Bindings.kt
+++ app/src/main/java/de/szalkowski/activitylauncher/services/Bindings.kt
@@ -4,27 +4,32 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.scopes.ActivityScoped
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(ActivityComponent::class)
 abstract class ServicesModule {
+    @ActivityScoped
     @Binds
     abstract fun bindActivityLauncherService(
         activityLauncherServiceImpl: ActivityLauncherServiceImpl
     ): ActivityLauncherService
 
+    @ActivityScoped
     @Binds
     abstract fun bindActivityListService(
         activityListServiceImpl: ActivityListServiceImpl
     ): ActivityListService
 
+    @ActivityScoped
     @Binds
     abstract fun bindPackageListService(
         packageListServiceImpl: PackageListServiceImpl
     ): PackageListService
 
+    @ActivityScoped
     @Binds
     abstract fun bindIconCreatorService(
         iconCreatorServiceImpl: IconCreatorServiceImpl
