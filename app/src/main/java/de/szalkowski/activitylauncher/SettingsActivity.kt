aefefbf1b1bc76bf2fe0bbf85b775a4543e2db98
Adam M. Szalkowski <adam@szalkowski.de>
2024-02-17 10:51:12 +0100

    fix(workaround): restart application on settings change

--- app/src/main/java/de/szalkowski/activitylauncher/SettingsActivity.kt
+++ app/src/main/java/de/szalkowski/activitylauncher/SettingsActivity.kt
@@ -3,9 +3,8 @@ package de.szalkowski.activitylauncher
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
-import de.szalkowski.activitylauncher.services.SettingsService
 import de.szalkowski.activitylauncher.ui.SettingsFragment
-import javax.inject.Inject
+
 
 @AndroidEntryPoint
 class SettingsActivity : AppCompatActivity() {
--- app/src/main/java/de/szalkowski/activitylauncher/ui/SettingsFragment.kt
+++ app/src/main/java/de/szalkowski/activitylauncher/ui/SettingsFragment.kt
@@ -1,7 +1,7 @@
 package de.szalkowski.activitylauncher.ui
 
+import android.content.Intent
 import android.content.SharedPreferences
-import android.content.res.Resources
 import android.os.Bundle
 import android.widget.Toast
 import androidx.preference.ListPreference
@@ -9,6 +9,7 @@ import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 import androidx.preference.SwitchPreference
 import dagger.hilt.android.AndroidEntryPoint
+import de.szalkowski.activitylauncher.MainActivity
 import de.szalkowski.activitylauncher.R
 import de.szalkowski.activitylauncher.services.RootDetectionService
 import de.szalkowski.activitylauncher.services.SettingsService
@@ -19,12 +20,33 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class SettingsFragment : PreferenceFragmentCompat() {
     private lateinit var prefs: SharedPreferences
+    private var needsRestart: Boolean = false
 
     @Inject
     internal lateinit var rootDetectionService: RootDetectionService
 
     @Inject
     internal lateinit var settingsService: SettingsService
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (!needsRestart) return
+
+        // workaround for applying settings by restarting app - PRs welcome
+        // FIXME reset the services state and reload affected activities
+        restartApp()
+    }
+
+    private fun restartApp() {
+        val intent = Intent(
+            this.requireContext(),
+            MainActivity::class.java,
+        )
+
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        this.startActivity(intent)
+        this.requireActivity().finishAffinity()
+    }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
@@ -71,6 +93,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
             Toast.makeText(activity, getText(R.string.warning_root_check), Toast.LENGTH_LONG).show()
         }
         prefs.edit().putBoolean("allow_root", newValue).apply()
+        needsRestart = true
         return true
     }
 
@@ -82,6 +105,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     private fun onHidePrivateUpdated(newValue: Boolean): Boolean {
         prefs.edit().putBoolean("hide_hide_private", newValue).apply()
+        needsRestart = true
         return true
     }
 
@@ -89,7 +113,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
         prefs.edit().putString("language", newValue).apply()
 
         settingsService.applyLocaleConfiguration(requireActivity().baseContext)
-        requireActivity().recreate()
+        needsRestart = true
         return true
     }
 }
