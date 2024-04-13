cbc1449da846f132cd78f40af9034d74c236fcdf
Adam M. Szalkowski <adam@szalkowski.de>
2024-04-13 11:25:30 +0200

    chore: upgradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,12 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.6")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
     }
 }
 
 plugins {
-    id("com.android.application") version "8.2.1" apply false
+    id("com.android.application") version "8.3.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.google.dagger.hilt.android") version "2.50" apply false
 }
