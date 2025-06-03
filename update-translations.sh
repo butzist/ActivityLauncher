ced1de33ef8214dcec2e60f34a5cbcbfb00bfbe7
Adam M. Szalkowski <adam@szalkowski.de>
2025-06-03 22:03:29 +0200

    chore: update translations

--- update-translations.sh
+++ update-translations.sh
@@ -2,8 +2,8 @@
 
 mkdir _crowdin
 pushd _crowdin
-wget https://crowdin.com/backend/download/project/activitylauncher.zip
-unzip activitylauncher.zip
+# wget https://crowdin.com/backend/download/project/activitylauncher.zip
+unzip ../ActivityLauncher.zip
 chmod 644 */*.*
 chmod 755 *
 rm -fr ../descriptions
