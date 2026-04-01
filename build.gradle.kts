5de2b064d4eac2bbf282872f75197f9d44616071
Adam M. Szalkowski <adam@szalkowski.de>
2026-04-01 17:31:19 +0200

    fix(ANR): critical note for firebase SDK

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,15 +4,15 @@ buildscript {
     }
     dependencies {
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
-        classpath("com.google.gms:google-services:4.4.1")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
-        classpath("com.google.firebase:perf-plugin:1.4.2")
+        classpath("com.google.gms:google-services:4.4.2")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:3.0.6")
+        classpath("com.google.firebase:perf-plugin:2.0.2")
     }
 }
 
 plugins {
     id("com.android.application") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-    id("com.google.dagger.hilt.android") version "2.50" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("com.google.dagger.hilt.android") version "2.55" apply false
     id("com.diffplug.spotless") version "6.25.0" apply false
 }
