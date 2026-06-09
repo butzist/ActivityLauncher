c387f43d82375642e3cf3ed49ea926a8b291284e
Adam M. Szalkowski <adam@szalkowski.de>
2026-06-09 08:10:03 +0200

    fix(Playwire): synchronize banner loading with SDK initialization

--- app/src/main/java/de/szalkowski/activitylauncher/services/AdService.kt
+++ app/src/main/java/de/szalkowski/activitylauncher/services/AdService.kt
@@ -1,11 +1,9 @@
 package de.szalkowski.activitylauncher.services
 
 import android.app.Activity
-import android.content.Context
 import android.view.ViewGroup
 
 interface AdService {
-    fun initialize(activity: Activity)
-    fun loadBanner(context: Context, container: ViewGroup)
-    fun destroyBanner()
+    fun loadBanner(activity: Activity, container: ViewGroup)
+    fun removeBanner(container: ViewGroup)
 }
--- app/src/noads/java/de/szalkowski/activitylauncher/services/AdServiceImpl.kt
+++ app/src/noads/java/de/szalkowski/activitylauncher/services/AdServiceImpl.kt
@@ -1,14 +1,12 @@
 package de.szalkowski.activitylauncher.services
 
 import android.app.Activity
-import android.content.Context
 import android.view.ViewGroup
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class AdServiceImpl @Inject constructor() : AdService {
-    override fun initialize(activity: Activity) {}
-    override fun loadBanner(context: Context, container: ViewGroup) {}
-    override fun destroyBanner() {}
+    override fun loadBanner(activity: Activity, container: ViewGroup) {}
+    override fun removeBanner(container: ViewGroup) {}
 }
