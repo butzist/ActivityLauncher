8abef299f22bb809daf2a52e80962d63cf462e92
Adam M. Szalkowski <adam@szalkowski.de>
2026-07-06 08:18:19 +0200

    chore: version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
             providers.environmentVariable("APPID").getOrElse("de.szalkowski.activitylauncher")
         minSdk = 16
         targetSdk = 37
-        versionCode = 20403
+        versionCode = 20405
         versionName = "2.4.0"
 
         vectorDrawables.useSupportLibrary = true
